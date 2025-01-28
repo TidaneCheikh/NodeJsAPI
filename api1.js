50fde995f8b5f7b4aa5b4dc873f533fa018f2a99
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-01-28 22:19:35 +0000

    m

--- api1.js
+++ api1.js
@@ -1,6 +1,5 @@
-function afficherSalut(){
-    console.log("afficher une valeur ")
-    console.log("attrapper")
-    return "the king "
+const tab =[2,3,4,1,3,45]
+for(let i=0;i<tab.length;i++){
+    console.log(tab[i])
 }
-afficherSalut()
+console.log(tab.sort())
