a095ebd4ed0c7e860d7b891ee2bd74bce98fa9f9
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-01-31 22:59:19 +0000

    dev

--- api1.js
+++ api1.js
@@ -1,12 +1,3 @@
-const tab = {
-    nom : 'cheikh',
-    prenom :'tidiane',
-    matiere : ['svt','math'],
-    afficher(){
-        console.log(this.nom)
-        console.log(this.matiere)
-    }
-}
-tab.afficher()
-const maping= [1,3,4,5,5,6,6,34,17,56,8]
-console.log(maping.filter((i)=> i%2==0))
+let a='7'
+let b=7
+console.log(a==b)
