5ea1d645b7dc4b1e43ed1eb959f8fd04a1cc6464
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-02-01 20:16:26 +0000

    dev1

--- api1.js
+++ api1.js
@@ -1,18 +1,11 @@
-function ask(question, yes, no) {
-    if (confirm(question)) yes()
-    else no();
-  }
-
-  function confirm(question){
-    return false;
-  }
-  
-  function showOk() {
-    console.log( "You agreed." );
-  }
-  
-  function showCancel() {
-    console.log( "You canceled the execution." );
-  }
-  
-  ask("Do you agree?", showOk, showCancel);
+let user={
+  nom:"cheikh"
+}
+let a= {}
+a=user
+console.log(a.nom)
+console.log(user.nom)
+console.log("-----------")
+a.nom="papa"
+console.log(a.nom)
+console.log(user.nom)
