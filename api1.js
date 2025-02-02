12ec7f6a10e61f1d970e2d3677627078c7ea9421
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-02-02 22:07:57 +0000

    s

--- api1.js
+++ api1.js
@@ -1,9 +1,2 @@
-/*Créez un objet calculatoravec trois méthodes :
-
-read()demande deux valeurs et les enregistre en tant que propriétés d'objet avec les noms aet brespectivement.
-sum()renvoie la somme des valeurs enregistrées.
-mul()multiplie les valeurs enregistrées et renvoie le résultat.*/
-let user ={}
-
-
-console.log(user?.address?.street)
+let id = Symbol("id");
+console.log(id.toString); // id
