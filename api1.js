e38e1225b3c414e26e3b07eb858921cf59baa50a
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-02-02 22:16:35 +0000

    f

--- api1.js
+++ api1.js
@@ -1,2 +1,11 @@
 let id = Symbol("id");
-console.log(id.toString); // id
+let user = {
+  name: "John",
+  age: 30,
+  [id]: 123
+};
+
+for (let key in user) alert(key); // name, age (no symbols)
+
+// the direct access by the symbol works
+console.log( "Direct: " + user[id] );
