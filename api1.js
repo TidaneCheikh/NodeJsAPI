4af8257c574abd3857e3d5c70b03e54009efd55b
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-02-02 21:43:03 +0000

    dc

--- api1.js
+++ api1.js
@@ -1,12 +1,9 @@
-function makeUser() {
-  return {
-    name: "John",
-    ref() {
-      return this;
-    }
-  };
-}
+/*Créez un objet calculatoravec trois méthodes :
 
-let user = makeUser();
+read()demande deux valeurs et les enregistre en tant que propriétés d'objet avec les noms aet brespectivement.
+sum()renvoie la somme des valeurs enregistrées.
+mul()multiplie les valeurs enregistrées et renvoie le résultat.*/
+let user ={}
 
-console.log( user.ref().name );
+
+console.log(user?.address?.street)
