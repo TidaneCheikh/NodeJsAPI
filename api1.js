dc38b85ebd8e4b688b44fd88e0a20bb531ce67fc
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-02-04 20:20:49 +0000

    tab1

--- api1.js
+++ api1.js
@@ -1,5 +1,5 @@
 chaine = "        chercher a plaire au gens    "
 console.log(chaine); 
-console.log(chaine.trim());
+console.log("chaine".repeat(7));
 console.log( "z".codePointAt(0) ); // 122
 console.log( "z".codePointAt(0).toString(16) );
