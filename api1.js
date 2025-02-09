975cd0315d34afa773eb38086a558e6f46ed1070
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-02-09 13:30:00 +0000

    vd

--- api1.js
+++ api1.js
@@ -1,14 +1,3 @@
-function multiplicationTable(size) {
-    let table = [];
-    for (let i = 1; i <= size; i++) {
-        let row = [];
-        for (let j = 1; j <= size; j++) {
-            row.push(i * j);
-        }
-        table.push(row);
-    }
-    return table;
-}
-
-// Exemple d'utilisation
-console.log(multiplicationTable(3));
+function getCount(str) {
+    return (str.match(/[aeiou]/ig)||[]).length;
+  }
