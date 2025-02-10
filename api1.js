359ddb362199b18e47a018f9f3411c4eb9886929
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-02-10 20:19:56 +0000

    flottant

--- api1.js
+++ api1.js
@@ -1,3 +1,17 @@
-function getCount(str) {
-    return (str.match(/[aeiou]/ig)||[]).length;
+function sumTwoSmallestNumbers(numbers) {  
+  let array=numbers.slice().sort((a, b) => a - b);
+  somme=0
+  let count=0
+  const isFloat = (number) => typeof number === "number" && number % 1 !== 0;
+  for(let i=0; i<array.length ;i++){
+    if(!isFloat(array[i])){
+      somme+=array[i]
+      count+=1
+      console.log(count);
+    }
+    if(count==2){
+      break
+    }
   }
+  return somme
+ }
