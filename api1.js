c2cd97367dd8b051da7d299f613d392ec321a259
Cheikhtidianthiam <cheikhtidiane15.thiam@ucad.edu.sn>
2025-02-12 21:16:22 +0000

    cd

--- api1.js
+++ api1.js
@@ -57,3 +57,24 @@ function findEvenIndex(arr){
   return -1
   //Code goes here!
 }
+
+function findOdd(A) {
+  const countOccurrences = (array, element) => array.filter((item) => item === element).length;
+  //happy coding!
+   let findindex=1;
+   let tem=1;
+   for(let i=0;i<A.length;i++){
+     if(countOccurrences(A,i)%2==0 && countOccurrences(A,i)>tem){
+        tem=countOccurrences(A,i)
+        findindex=A[i]
+     }
+    
+  }
+  return findindex;
+}
+const countOccurrences = (array, element) => array.filter((item) => item === element).length;
+console.log(countOccurrences([20,1,-1,2,-2,3,3,5,5,1,2,4,20,1,1,1,1,4,-1,-2,5],1));
+
+  //happy coding!
+
+//console.log(findOdd([1,2,1,1,2,2]));
